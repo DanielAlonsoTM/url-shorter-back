4f6c9c8777f5f7cb24f7f116439d5ea7a84a28e6
Daniel Tapia Maldonado <dtapia@klap.cl>
2025-08-20 01:21:27 -0400

    feat: Constants utilities added

---
+++ src/main/java/com/devbook/url_shorter/url/utils/Constants.java
@@ -0,0 +1,7 @@
+package com.devbook.url_shorter.url.utils;
+
+public class Constants {
+    public static String URL_BASE = "https://www.shorter.com/";
+    public static Long MONTHS_TO_EXPIRE = 6L;
+    public static int LENGTH_URL = 6;
+}
